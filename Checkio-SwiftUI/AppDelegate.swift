eee3f7667890d5a2f55c7675a516b7b988295a76
ajay.yan <ajay.yan@anker.io>
2023-04-19 16:47:18 +0800

    [需求] 解决pod改SPM 引用库的问题

--- Checkio-SwiftUI/AppDelegate.swift
+++ Checkio-SwiftUI/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SwiftUI
 import NXNavigationExtension
+import NXNavigationExtensionSwiftUI
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- Checkio-SwiftUI/UIFunction/Home/View/RoundedCorners.swift
+++ Checkio-SwiftUI/UIFunction/Home/View/RoundedCorners.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import SwiftUIPathAnimations
 
 struct RoundedCorners: View {
     var color: Color = .blue
